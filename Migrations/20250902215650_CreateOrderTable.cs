467fe4a77e7e6f093bbb5be4adc426bbb8c80f35
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2025-09-02 19:13:27 -0300

    chore: add first migration to create orders table

---
+++ Migrations/20250902215650_CreateOrderTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace order_management_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Cliente = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Produto = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Valor = table.Column<decimal>(type: "numeric", nullable: false),
+                    Status = table.Column<int>(type: "integer", maxLength: 20, nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
