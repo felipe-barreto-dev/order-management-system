cffe52f7a9e7dfcc51eebbe078e1539ddf4b6de1
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2025-09-02 19:12:49 -0300

    feat: add db context

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
