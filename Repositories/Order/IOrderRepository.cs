289714435ec2aa268ed085183980798968d66e16
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2025-09-14 14:53:35 -0300

    refact: OrderRepository to use BaseRepository

--- Repositories/Order/IOrderRepository.cs
+++ Repositories/Order/IOrderRepository.cs
@@ -1,13 +1,10 @@
+using OrderManagementSystem.Enums;
 using OrderManagementSystem.Models;
+using OrderManagementSystem.Repositories.Base;
 
-namespace OrderManagementSystem.Repositories.Order
+namespace OrderManagementSystem.Repositories.Order;
+
+public interface IOrderRepository : IBaseRepository<OrderModel, Guid>
 {
-    public interface IOrderRepository
-    {
-        Task<OrderModel> CreateOrderAsync(OrderModel order);
-        Task<OrderModel?> GetOrderByIdAsync(Guid id);
-        Task<List<OrderModel>> GetAllOrdersAsync();
-        Task<bool> UpdateOrderStatusAsync(Guid id, Enums.OrderStatusEnum status);
-        Task<bool> DeleteOrderAsync(Guid id);
-    }
+    Task<bool> UpdateOrderStatusAsync(Guid id, OrderStatusEnum status);
 }
